b46a2d7cbce71b3e1708965ee3e089ad4d7df9ab
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-04-23 17:31:08 +0100

    Added Intents handling for default, Hello & Goodbye

---
+++ TechSupport/Code/TechSupport/Dialog/Phrases.cs
@@ -0,0 +1,11 @@
+﻿namespace TechSupport.Dialog
+{
+    public class Phrases
+    {
+        public const string HELLO = "Hello, it's IT. How can I help you?";
+        public const string GOODBYE = "You are welcome, then!";
+
+        public const string START_OVER = "I guess you want to start over. Let's try this again.";
+        public const string DIDNT_GET_THAT = "Sorry, didn't get that! Could you rephrase.";
+    }
+}
--- TechSupport/Code/TechSupport/Dialog/TechSupportDialog.cs
+++ TechSupport/Code/TechSupport/Dialog/TechSupportDialog.cs
@@ -4,6 +4,7 @@ using Microsoft.Bot.Builder.Luis.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -16,7 +17,30 @@ namespace TechSupport.Dialog
         [LuisIntent("")]
         public async Task None(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("Hello IT?");
+            await DidntGetThat(context);
+        }
+
+        private async Task DidntGetThat(IDialogContext context)
+        {
+            await Respond(context, Phrases.DIDNT_GET_THAT);
+        }
+
+        [LuisIntent("Hello")]
+        public async Task Hello(IDialogContext context, LuisResult result)
+        {
+            await Respond(context, Phrases.HELLO);
+        }
+
+        [LuisIntent("Bye")]
+        public async Task Bye(IDialogContext context, LuisResult result)
+        {
+            await Respond(context, Phrases.GOODBYE);
+        }
+
+        private async Task Respond(IDialogContext context, string message, int sleep = 400)
+        {
+            Thread.Sleep(sleep);
+            await context.PostAsync(message);
             context.Wait(MessageReceived);
         }
     }
